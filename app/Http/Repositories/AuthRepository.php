d447abd9ad35c4e96603582e60987932bbc889f9
Petar Popovic <petar.popovic@icloud.com>
2022-03-05 16:16:32 +0100

    Create function for storing user in database

--- app/Http/Repositories/AuthRepository.php
+++ app/Http/Repositories/AuthRepository.php
@@ -3,12 +3,18 @@
 namespace App\Http\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class AuthRepository
 {
-    public function createUser(): User
+    public function createUser(array $data): Builder|Model
     {
-
+        return User::query()->create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
     }
-
 }
