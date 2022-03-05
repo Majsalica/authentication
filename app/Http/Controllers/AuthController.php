9a9142b704ee4b2a718a784c8941e4f071925702
Petar Popovic <petar.popovic@icloud.com>
2022-03-05 15:16:17 +0100

    Add dependencies in constructors.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,14 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\AuthService;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct(
+        private AuthService $authService,
+    ) {
+    }
 }
--- app/Http/Services/AuthService.php
+++ app/Http/Services/AuthService.php
@@ -1,8 +1,14 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Http\Services;
 
+use App\Http\Repositories\AuthRepository;
+
 class AuthService
 {
+    public function __construct(
+        private AuthRepository $authRepository,
+    ) {
+    }
 
 }
