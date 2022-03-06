72f095418f0d5b28577625a18b8494b20c0ba2d2
Petar Popovic <petar.popovic@icloud.com>
2022-03-06 15:48:06 +0100

    Add properties in comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property-read $email;
+ * @property-read $password
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
